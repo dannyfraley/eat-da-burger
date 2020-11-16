7151abeab2d3e890f8e3f2fbefc1c59b1dd5eaee
Danny Fraley <dannyfraley@gmail.com>
2020-11-16 12:54:41 -0500

    completed directory structure

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('I WISH I WAS DEAD BURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('DEPRESSED FLAMINGO BURGER', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('THE INCREDIBLE SULK BURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('HEY BUDDY, LOVE THE BURGER', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('IS THAT A HUMAN BEING BURGER', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('LEGO MY EGGO BURGER', true);
