a731f530d248b64c201f2c01b37d3bc92a2fbd07
Danny Fraley <dannyfraley@gmail.com>
2020-11-17 22:21:19 -0500

    more css tweaks

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,7 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
+CREATE TABLE burgers(
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
